25e9e945f7907c56e7533cc08958a3a303c8b460
bcp1-admin <te.takada@infocom.co.jp>
2024-06-20 00:33:42 +0900

    ０６２０作業

--- TFApp_before/Program.cs
+++ TFApp_before/Program.cs
@@ -6,14 +6,27 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<TFAppContext>(options =>
     options.UseInMemoryDatabase(builder.Configuration.GetConnectionString("TFAppContext")));
 
-builder.Services.AddDistributedMemoryCache();
-
+//分散 Redis キャッシュの構成を追加する
+//builder.Services.AddDistributedMemoryCache();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("RedisConnection");
+    options.InstanceName = builder.Environment.EnvironmentName.ToLower();
+});
 // セッションの設定
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(5);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddHttpContextAccessor();
+
+//Program.cs に名前付き HTTP クライアントを登録する
+builder.Services.AddHttpClient("weather", httpClient =>
+{
+    httpClient.BaseAddress = new Uri("https://fnappxsua4qa2x7s7o.azurewebsites.net/");
 });
 
 var app = builder.Build();
